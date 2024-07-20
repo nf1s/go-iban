16c363847d88164e188dd07c1c6954478b64448f
Ahmed Nafies <ahmed.nafies@gmail.com>
2024-07-20 12:05:30 +0200

    fix: fixes indentation

--- migrations/1_create_iban_table.up.sql
+++ migrations/1_create_iban_table.up.sql
@@ -2,8 +2,8 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS iban (
 	countryCode		char(2) PRIMARY KEY NOT NULL,
-	country	 			varchar NOT NULL,
-	size					int NOT NULL,
+	country	 		varchar NOT NULL,
+	size			int NOT NULL,
 	BBANFormat	 	varchar NOT NULL,
 	IBANFormat	 	varchar NOT NULL
 );
